f240b5d61ffcfd059505407f610e195f73ae7850
zishanHow <111298322+zishanHow@users.noreply.github.com>
2022-11-13 03:08:31 +0600

    change the UI/UX design fully
    and added different functionality,
    but still hasn't start work with the js so that is the reason i haven't delete the previous design. i will have to work with js. next
    
    and might be it not fully responsive so i might back to it again for the design.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const passLength = document.getElementById("pass-length")
 
 passOne.textContent = "👀 👀"
 passTwo.textContent = "👀 👀"
-suportMessage.textContent = "Never use an insecure password again"
 
 //   === characters for password === 
 const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
